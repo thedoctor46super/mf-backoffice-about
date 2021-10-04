b919cb831046380f88027e9b0b3c46e8f859c981
Maurizio Paletti <maurizio.paletti@fasttrack-solutions.com>
2021-10-04 12:37:20 +0200

    first commit

---
+++ src/components/About.vue
@@ -0,0 +1,17 @@
+<template>
+  <h1>backoffice-about</h1>
+  <button @click="count++">count is: {{ count }}</button>
+  <p>Edit <code>components/About.vue</code> to test hot module replacement.</p>
+  <router-link to="/">Home</router-link>
+</template>
+
+<script>
+export default {
+  name: 'About',
+  data() {
+    return {
+      count: 0,
+    };
+  },
+};
+</script>
